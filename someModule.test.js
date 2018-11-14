df79c91670af8d485a1b5b17b499ed08fc554c93
MaxSentineri <m.sentineri@studenti.unitn.it>
2018-11-14 15:17:24 +0100

    primo commit

---
+++ someModule.test.js
@@ -0,0 +1,21 @@
+const conc = require('./modules').conc
+
+test('multiply 2', () => {
+    expect(4).toBe(4);
+});
+
+test('concat test', () => {
+    expect(conc('a','b')).toBe('ab');
+});
+
+test('concat null', () => {
+    expect(conc(null,null)).toBe(0);
+});
+
+
+const fetch = require("node-fetch")
+test('fetch', async() => {
+  var r = await fetch("https://sites.google.com/unitn.it/seii-inf-org-unitn").then( (r) => {
+    expect(r.status).toEqual(200)
+  })
+})
